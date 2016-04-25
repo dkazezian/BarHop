5b59dc67f0a29ea982e76cc935a4c7438db76102
Dan Kazezian <kazezian@rams.colostate.edu>
2016-04-25 16:40:58 -0600

    Added bar profile base structure

---
+++ app/src/main/java/cs464/barhop/Bar_profile.java
@@ -0,0 +1,13 @@
+package cs464.barhop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bar_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_profile);
+    }
+}
