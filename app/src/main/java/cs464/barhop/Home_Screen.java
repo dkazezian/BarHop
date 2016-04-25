2cfc6810f97459ee9c1bd0543c8d5cf0bee9bb9c
Sean <seanmann17@gmail.com>
2016-04-25 16:24:25 -0600

    Modified search page and search java

--- app/src/main/java/cs464/barhop/Home_Screen.java
+++ app/src/main/java/cs464/barhop/Home_Screen.java
@@ -2,6 +2,7 @@ package cs464.barhop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class Home_Screen extends AppCompatActivity {
 
