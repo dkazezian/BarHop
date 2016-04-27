26f3a403f69d530b755423466b7717ec3df818b8
Sean <seanmann17@gmail.com>
2016-04-27 17:09:12 -0600

    Added search method to search page

--- app/src/main/java/cs464/barhop/Search.java
+++ app/src/main/java/cs464/barhop/Search.java
@@ -1,5 +1,6 @@
 package cs464.barhop;
-
+import java.io.*;
+import java.util.Scanner;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Spinner;
@@ -13,48 +14,153 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.EditText;
 //Example found here: http://www.tutorialspoint.com/android/android_spinner_control.htm
-public class Search extends Activity implements OnItemSelectedListener{
+public class Search extends Activity {
+    EditText etext;
+    Spinner spinner;
+    String selection;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
 
-        Spinner spinner = (Spinner) findViewById(R.id.day_spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
 
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("Automobile");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
 
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                selection = spinner.getSelectedItem().toString();
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+            }
 
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-    }
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                selection="-1";
+            }
+        });
     }
 
+    public void searchalgorithm(String day){
+        ArrayList<Integer> results=new ArrayList<Integer>();
+        String filename = "/BarHop/app/assets/barpageexample"; //Database filename
+        String line = null;
+        etext = (EditText) findViewById(R.id.editText);
+        String searchstring = etext.getText().toString();
+        System.out.print("The text is " + searchstring);
+        String delims = "[ ]+";
+        String[] tokens = searchstring.split(delims);
+        for (int i = 0; i<tokens.length; i++){//Get search token so have to search database
+            System.out.print("token is " + tokens[i]);
+            int linecount = 0;
+            int barnamelinenumber=0;
+            Scanner s = null;
+            Scanner s2 = null;
+            try {
+                s = new Scanner(new BufferedReader(new FileReader(filename)));
+                while (s.hasNextLine()) {
+                    String fullline=s.nextLine();
+                    s2 = new Scanner(fullline);
+                    barnamelinenumber=((linecount/10)*10)+1;
+                    while(s2.hasNext()){
+                        String word= s2.next();
+                        if (tokens[i].equals(word)) {
+                            if (!results.contains(barnamelinenumber)) {
+                                if(day.equals("-1")) {
+                                    results.add(barnamelinenumber);
+                                }
+                                else{
+                                    int specialslinenumber=barnamelinenumber+7;
+                                    int daynumber=0;
+                                    switch(day) {
+                                        case "Mon":
+                                            daynumber=1;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Tue":
+                                            daynumber=2;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Wed":
+                                            daynumber=3;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Thur":
+                                            daynumber=4;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Fri":
+                                            daynumber=5;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Sat":
+                                            daynumber=6;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                        case "Sun":
+                                            daynumber=7;
+                                            if (findspecialOnDay(specialslinenumber, daynumber)) results.add(barnamelinenumber);
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    linecount++;
+                }
+            } catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
 
+        }
+    }
+    private boolean findspecialOnDay(int linenumber, int day){
+        boolean bool=false;
+        String specialsline="";
+        String filename = "/BarHop/app/assets/barpageexample";
+        Scanner s = null;
+        Scanner s2=null;
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(filename)));
+            for (int i=0; i<linenumber; i++){
+                s.nextLine();
+            }
+            specialsline=s.nextLine();
+            s2 = new Scanner(specialsline).useDelimiter(";");
+            for (int k = 0; k<day; k++){
+                s2.next();
+            }
+            if (s2.next().equals("None")){
+                bool = false;
+            }
+            else bool = true;
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return bool;
+    }
+
+    private String findBarNameByLineNumber(int linenumber){
+        String barname="";
+        String filename = "/BarHop/app/assets/barpageexample";
+        Scanner s = null;
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(filename)));
+            for (int i=0; i<linenumber; i++){
+                s.nextLine();
+            }
+            barname=s.nextLine();
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return barname;
+    }
 }
