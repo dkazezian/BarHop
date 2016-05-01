df7650f3c36285018bea0516cab573a6de8b6f48
nickzeus <nickz1994@comcast.net>
2016-05-01 16:07:37 -0600

    hopefully added list items to favorites screen. should only be the objects in the myFavorites.txt

--- app/src/main/java/cs464/barhop/Favorites.java
+++ app/src/main/java/cs464/barhop/Favorites.java
@@ -1,11 +1,19 @@
 package cs464.barhop;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,17 +22,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
-public class Favorites extends AppCompatActivity {
+public class Favorites extends ListActivity {
     public int lineNumber;
     public String BarName;
-  //  public String[] Bars;
-   // private List<Favorite>;
+    //  public String[] Bars;
+    // private List<Favorite>;
     public ArrayList<String> values;
     private InputStream reader;
     private BufferedReader input;
@@ -34,22 +40,22 @@ public class Favorites extends AppCompatActivity {
     //public File myFavorites = "myFavorites.txt";
 
 
-    private void loadFavorites(){
+    private void loadFavorites() {
         Scanner scan = new Scanner(reader);
         try {
             String line = input.readLine();
 
-            while (line!= null) {
+            while (line != null) {
                 BarName = scan.next();
                 lineNumber = scan.nextInt();
-                Favorite temp = new Favorite(BarName,lineNumber);
+                Favorite temp = new Favorite(BarName, lineNumber);
                 favorites.add(temp);
                 selected.add(false);
 
 
                 line = scan.nextLine();
             }
-        }catch( IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -63,26 +69,26 @@ public class Favorites extends AppCompatActivity {
         boolean p;
         String REMOVE;
         ArrayList<Integer> del = new ArrayList<>();
-     //   ArrayList<String> re = new ArrayList<>();
-        for(int i =0; i<selected.size();i++){
+        //   ArrayList<String> re = new ArrayList<>();
+        for (int i = 0; i < selected.size(); i++) {
             p = selected.get(i);
-            if( p){
-                REMOVE =favorites.get(i).Name +" "+ favorites.get(i).LineNum;
+            if (p) {
+                REMOVE = favorites.get(i).Name + " " + favorites.get(i).LineNum;
                 removeLineFromFile(REMOVE);
                 favorites.remove(i);
                 del.add(i);
             }
         }
-        for(int k = del.size(); k>0;k--) {
+        for (int k = del.size(); k > 0; k--) {
             selected.remove(k);
 
         }
 
     }
-    private void addFavorite(String Nam,int barId){
+
+    private void addFavorite(String Nam, int barId) {
 
         try {
-
 
 
             if (!myFavorites.isFile()) {
@@ -97,11 +103,11 @@ public class Favorites extends AppCompatActivity {
             PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
 
             String line;
-            Favorite Create=new Favorite(Nam,barId);
+            Favorite Create = new Favorite(Nam, barId);
             //Read from the original file and write to the new
             //unless content matches data to be removed.
             while ((line = br.readLine()) != null) {
-                if(favorites.contains(Create)){
+                if (favorites.contains(Create)) {
                     return;
                 }
 
@@ -117,25 +123,23 @@ public class Favorites extends AppCompatActivity {
             }
 
             //Rename the new file to the filename the original file had.
-           // if (!tempFile.renameTo(myFavorites))
-                //System.out.println("Could not rename file");
+            // if (!tempFile.renameTo(myFavorites))
+            //System.out.println("Could not rename file");
 
-        }
-        catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    public void removeLineFromFile( String lineToRemove) {
+
+    public void removeLineFromFile(String lineToRemove) {
 
         try {
 
 
-
             if (!myFavorites.isFile()) {
-                System.out.println("Parameter is not an existing file");
+                //System.out.println("Parameter is not an existing file");
                 return;
             }
 
@@ -162,51 +166,56 @@ public class Favorites extends AppCompatActivity {
 
             //Delete the original file
             if (!myFavorites.delete()) {
-                System.out.println("Could not delete file");
+                //System.out.println("Could not delete file");
                 return;
             }
 
             //Rename the new file to the filename the original file had.
             if (!tempFile.renameTo(myFavorites))
-                System.out.println("Could not rename file");
+               // System.out.println("Could not rename file");
 
-        }
-        catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         try {
 
             reader = getAssets().open("myFavorites.txt");
             input = new BufferedReader(new InputStreamReader(reader));
             loadFavorites();
-            for(int i=0;i<favorites.size();i++) {
-                BarName = favorites.get(i).Name;
-                values.add(BarName);
-            }
+            values = getFavorites();
             //BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        String[] barNames = new String[values.size()];
+        for (int i = 0; i < values.size(); i++) {
+            barNames[i] = values.get(i);
+        }
+        ArrayAdapter<String> faveAdapter= new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_selectable_list_item, barNames);
+        getListView().setAdapter((ListAdapter) faveAdapter);
+
 
         setContentView(R.layout.activity_favorites);
 
     }
-    private List<Favorite> getFavorites() {
-        List<Favorite> list = new ArrayList<>();
-        for(int i=0;i<favorites.size();i++) {
-            list.add(favorites.get(i));
+
+    private ArrayList<String> getFavorites() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < favorites.size(); i++) {
+            list.add(favorites.get(i).Name);
         }
         // Initially select one of the items
-       // list.get(1).setSelected(true);
+        // list.get(1).setSelected(true);
         return list;
     }
 
