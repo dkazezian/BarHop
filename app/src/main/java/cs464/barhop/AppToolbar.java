decb42d3fc9d6b7f410f3679bd879329f10beb66
Dan Kazezian <kazezian@rams.colostate.edu>
2016-05-02 15:42:16 -0600

    added toolbar functionality

--- app/src/main/java/cs464/barhop/AppToolbar.java
+++ app/src/main/java/cs464/barhop/AppToolbar.java
@@ -1,41 +1,37 @@
 package cs464.barhop;
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class Home_Screen extends AppCompatActivity {
-
-
-    private Toolbar apptoolbar;
-
-
-
-
-
+/**
+ * Created by kazezian on 5/2/16.
+ */
+public class AppToolbar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home__screen);
-        final Button search = (Button) findViewById(R.id.search);
-        final Button nearbyme = (Button) findViewById(R.id.nearbyMe);
-        final Button favorites = (Button) findViewById(R.id.favorites);
+        setContentView(R.layout.toolbar);
+
+        final Button search = (Button) findViewById(R.id.bottom_bar_search);
+        final Button nearbyme = (Button) findViewById(R.id.bottom_bar_nearby);
+        final Button favorites = (Button) findViewById(R.id.bottom_bar_favorites);
         search.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent activityChangeIntent = new Intent(Home_Screen.this, Search.class);
-                Home_Screen.this.startActivity(activityChangeIntent);
+                Intent activityChangeIntent = new Intent(AppToolbar.this, Search.class);
+                AppToolbar.this.startActivity(activityChangeIntent);
             }
         });
 
         nearbyme.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(Home_Screen.this, Searchresults.class);
+                Intent intent = new Intent(AppToolbar.this, Searchresults.class);
                 ArrayList<Integer> results= new ArrayList<Integer>();
                 results.add(2);
                 results.add(12);
@@ -43,21 +39,17 @@ public class Home_Screen extends AppCompatActivity {
                 Bundle b = new Bundle();
                 b.putIntegerArrayList("searchresults", results);
                 intent.putExtras(b);
-                Home_Screen.this.startActivity(intent);
+                AppToolbar.this.startActivity(intent);
                 finish();
             }
         });
 
         favorites.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent activityChangeIntent = new Intent(Home_Screen.this, Favorites.class);
-                Home_Screen.this.startActivity(activityChangeIntent);
+                Intent activityChangeIntent = new Intent(AppToolbar.this, Favorites.class);
+                AppToolbar.this.startActivity(activityChangeIntent);
             }
         });
-
-        /*apptoolbar=(AppToolbar)findViewById((R.id.toolbar));
-        setSupportActionBar(apptoolbar);*/
-
     }
 
 }
